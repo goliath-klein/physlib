032812c169d8e0e8e8a7a96cf6b264efc5cc09ff
David Gross <david.gross@thp.uni-koeln.de>
2026-08-31 12:31:35 +0200

    ...

--- Physlib/QuantumMechanics/Basic/States.lean
+++ Physlib/QuantumMechanics/Basic/States.lean
@@ -12,25 +12,47 @@ public import Physlib.QuantumMechanics.Basic.PositiveLinearMap.Restrict
 
 # States
 
+Some definitions on states.
+
+This file is a stub.
+
 -/
 
 @[expose] public section
 
-/-- State space of a complex ordered vector space with unit -/
-notation " 𝓢[" A₁ "] " => A₁ →ₚ₁[ℂ] ℂ
+/-- Positive linear functionals on a complex ordered vector space -/
+notation " 𝓟[" A "] " => A →ₚ[ℂ] ℂ
 
-open Complex ComplexOrder
+/-- State space of a complex ordered vector space with unit -/
+notation " 𝓢[" A "] " => A →ₚ₁[ℂ] ℂ
+
+open Complex ComplexOrder ContinuousLinearMap
 open scoped InnerProductSpace
 
 variable {H : Type*} [NormedAddCommGroup H] [InnerProductSpace ℂ H]
 
-noncomputable def vectorState (ψ : H) (h : ‖ψ‖ = 1) : 𝓢[H →L[ℂ] H] where
+@[simps apply]
+noncomputable def PositiveLinearMap.ofVec (ψ : H) : 𝓟[H →L[ℂ] H] where
   toFun x := ⟪ψ, x • ψ⟫_ℂ
   map_add' x y := by simp
   map_smul' x y := by simp
-  map_one' := by simp [h]
   monotone' x y hxy := by
-    simp
-    -- have h := sub_nonneg.mpr hxy
-    -- have hψ := congrArg (fun z => ⟪ψ, z ψ⟫_ℂ) h
-    -- simpa [map_sub] using hψ
+    simpa [inner_sub_right] using ((le_def x y).mp hxy).inner_nonneg_right ψ
+
+@[simps! apply]
+noncomputable def UnitalPositiveLinearMap.ofVec {ψ : H} (h : ‖ψ‖ = 1) : 𝓢[H →L[ℂ] H] :=
+  { PositiveLinearMap.ofVec ψ with map_one' := by simp [h] }
+
+section Example
+
+open UnitalPositiveLinearMap
+
+-- TBD: Currently required for `StarRing (H →L[ℂ] H)`. Should not be necessary.
+variable [CompleteSpace H]
+
+/-- Version with real-valued results on self-adjoint elements. -/
+example (ψ : H) (h : ‖ψ‖ = 1) :
+    (ofVec h).restrictSAC (1 : selfAdjoint (H →L[ℂ] H)) = (1 : ℝ) := by
+  simp
+
+end Example
