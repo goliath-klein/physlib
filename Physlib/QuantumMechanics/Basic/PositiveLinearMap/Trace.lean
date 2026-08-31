7a59f6b528574c22dc6d6b3529fa5626ff44596f
David Gross <david.gross@thp.uni-koeln.de>
2026-09-01 00:10:22 +0200

    cleanup

--- Physlib/QuantumMechanics/Basic/PositiveLinearMap/Trace.lean
+++ Physlib/QuantumMechanics/Basic/PositiveLinearMap/Trace.lean
@@ -14,8 +14,9 @@ public import Mathlib
 
 ## Main definitions
 
-- `PositiveLinearMap.restrict`: The restriction of a positive linear map to a submodule
-- `UnitalPositiveLinearMap.restrict`: The restriction of a unital positive linear map to a submodule
+- `PositiveLinearMap.conjugateₚ` Conjugation `x ↦ c * x * star x`, as a positive linear map
+- `ContinuousLinearMap.traceₚ` The trace on continuous linear maps, as a positive linear map
+- `ContinuousLinearMap.traceMulOpₚ`: `x ↦ tr √ρ * x * star √ρ` as a positive linear map
 
 -/
 
@@ -37,21 +38,6 @@ end Conjugate
 
 open ComplexOrder
 
--- section RCLike
---
--- variable {𝕜 E : Type*} [RCLike 𝕜] [NormedAddCommGroup E] [InnerProductSpace 𝕜 E] [CompleteSpace E]
---
--- @[simps!]
--- noncomputable def ContinuousLinearMap.traceₚ : (E →L[𝕜] E) →ₚ[𝕜] 𝕜 := .mk₀
---     { toFun x := x.toLinearMap.trace 𝕜 E
---       map_add' x y := by simp
---       map_smul' m x := by simp }
---     ( fun x h ↦ by
---         rw [← coe_le_coe_iff, toLinearMap_zero] at h
---         simpa using sub_zero x ▸ h.trace_nonneg )
---
--- end RCLike
-
 section Complex
 
 variable {E : Type*} [NormedAddCommGroup E] [InnerProductSpace ℂ E] [CompleteSpace E]
@@ -69,8 +55,6 @@ noncomputable def traceₚ : (E →L[ℂ] E) →ₚ[ℂ] ℂ := .mk₀
       map_smul' m x := by simp }
     ( fun x h ↦ by
         simpa using (x.isPositive_toLinearMap_iff.mpr (x.nonneg_iff_isPositive.mp h)).trace_nonneg )
---        simp_all [nonneg_iff_isPositive, ← isPositive_toLinearMap_iff,
---          LinearMap.IsPositive.trace_nonneg] )
 
 open PositiveLinearMap
 
@@ -84,10 +68,12 @@ theorem traceMulOpₚ_apply_of_nonneg {ρ : E →L[ℂ] E} (h : 0 ≤ ρ) (x : E
   simp_rw [traceMulOpₚ, PositiveLinearMap.comp_apply, coe_toLinearMap, conjugateₚ_apply,
     traceₚ_apply, toLinearMap_mul]
   rw [mul_assoc, LinearMap.trace_mul_comm, mul_assoc, (CFC.sqrt_nonneg ρ).isSelfAdjoint.star_eq]
-  have := congrArg ContinuousLinearMap.toLinearMap (CFC.sqrt_mul_sqrt_self ρ h)
+  have := congrArg toLinearMap (CFC.sqrt_mul_sqrt_self ρ h)
   simp_all
 
 @[simp]
 theorem traceMulOpₚ_apply_of_not_nonneg {ρ : E →L[ℂ] E} (h : ¬0 ≤ ρ) (x : E →L[ℂ] E) :
       ρ.traceMulOpₚ x = 0 := by
   simp [traceMulOpₚ, CFC.sqrt_of_not_nonneg h]
+
+end ContinuousLinearMap
