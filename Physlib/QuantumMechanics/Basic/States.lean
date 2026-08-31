bf84c1a4b4b6a0bd1c976d0076c0a458ed92d952
David Gross <david.gross@thp.uni-koeln.de>
2026-08-31 13:03:59 +0200

    ...

--- Physlib/QuantumMechanics/Basic/States.lean
+++ Physlib/QuantumMechanics/Basic/States.lean
@@ -25,13 +25,13 @@ section Notation
 /-- Positive linear functionals on an ordered `𝕜`-vector space -/
 notation " 𝓟[" 𝕜 ", " A "] " => A →ₚ[𝕜] 𝕜
 
-/-- Positive linear functionals on a complex ordered vector space -/
+/-- Positive linear functionals on an ordered complex vector space -/
 notation " 𝓟[" A "] " => A →ₚ[ℂ] ℂ
 
 /-- State space of an ordered `𝕜`-vector space with unit -/
 notation " 𝓢[" 𝕜 ", " A "] " => A →ₚ₁[𝕜] 𝕜
 
-/-- State space of a complex ordered vector space with unit -/
+/-- State space of an ordered complex vector space with unit -/
 notation " 𝓢[" A "] " => A →ₚ₁[ℂ] ℂ
 
 end Notation
@@ -44,7 +44,7 @@ open scoped InnerProductSpace
 variable {H 𝕜 : Type*} [RCLike 𝕜] [NormedAddCommGroup H] [InnerProductSpace 𝕜 H]
 
 @[simps apply]
-noncomputable def PositiveLinearMap.ofVec (ψ : H) : 𝓟[𝕜, H →L[𝕜] H] where
+def PositiveLinearMap.ofVec (ψ : H) : 𝓟[𝕜, H →L[𝕜] H] where
   toFun x := ⟪ψ, x • ψ⟫_𝕜
   map_add' x y := by simp [inner_add_right]
   map_smul' x y := by simp [inner_smul_right]
@@ -52,7 +52,7 @@ noncomputable def PositiveLinearMap.ofVec (ψ : H) : 𝓟[𝕜, H →L[𝕜] H] 
     simpa [inner_sub_right] using ((le_def x y).mp hxy).inner_nonneg_right ψ
 
 @[simps! apply]
-noncomputable def UnitalPositiveLinearMap.ofVec {ψ : H} (h : ‖ψ‖ = 1) : 𝓢[𝕜, H →L[𝕜] H] :=
+def UnitalPositiveLinearMap.ofVec {ψ : H} (h : ‖ψ‖ = 1) : 𝓢[𝕜, H →L[𝕜] H] :=
   { PositiveLinearMap.ofVec ψ with map_one' := by simp [h] }
 
 end ofVec
